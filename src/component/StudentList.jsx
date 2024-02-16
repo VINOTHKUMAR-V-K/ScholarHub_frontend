fd046ba92fda709e7f48027dda5a3d05d36e0516
NAVEEN P <110113905+Naveen-405@users.noreply.github.com>
2024-02-16 11:12:34 +0530

    Update StudentList.jsx

--- src/component/StudentList.jsx
+++ src/component/StudentList.jsx
@@ -1,33 +1,54 @@
-import axios from 'axios'
-import React, { useEffect, useState } from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 
 export const StudentList = () => {
-    const time= new Date()
-    const [student,setStudent]=useState([])
-    useEffect(()=>{
-        axios.get(`http://localhost:5000/getAllStudent`)
-        .then((res)=>setStudent(res.data))
-        .catch((err)=>err)
-    })
-		return (
-        <div className='p-5'> 
-           <h1 className='fw-bold'>All Students List</h1>
-           <hr />
-			{student.map(stu=>{
-                return <div class="card-group">
-				<div class="card">
-					<h5 class="card-title">{`${stu.firstname} ${stu.lastname}`}</h5>
-					<p class="card-text">{stu.email}</p>
-                    <p class="card-text">{stu.dob}</p>
-                    <p class="card-text">{stu.address}</p>
-                    <p class="card-text">{stu.state}</p>
-				</div>
-				<div class="card-footer">
-					<small class="text-muted">{`Last updated ${time} ago`}</small>
-				</div>
-			</div>
-            })}
-		</div>
+  const time = new Date();
+  const [student, setStudent] = useState([]);
 
-	)
-}
+  useEffect(() => {
+    axios
+      .get(`http://localhost:5000/getAllStudent`)
+      .then((res) => setStudent(res.data))
+      .catch((err) => console.error(err));
+  }, []);
+
+  const deleteBtn = (index) => {
+    // Remove the card data at the specified index
+    const updatedStudent = [...student];
+    updatedStudent.splice(index, 1);
+    setStudent(updatedStudent);
+
+    // Remove session storage "deleterow" data
+    localStorage.removeItem("deletedRow");
+
+    // Add session storage "status" => "rejected"
+    localStorage.setItem("status", "Deleted");
+  };
+
+  return (
+    <div className='p-5'>
+      <h1 className='fw-bold'>All Students List</h1>
+      <hr />
+      {student.map((stu, index) => (
+        <div className='card-group' key={index}>
+          <div className='card' id='row'>
+            <h5 className='card-title'>{`${stu.firstname} ${stu.lastname}`}</h5>
+            <p className='card-text'>{stu.email}</p>
+            <p className='card-text'>{stu.dob}</p>
+            <p className='card-text'>{stu.address}</p>
+            <p className='card-text'>{stu.state}</p>
+            <button
+              className='btn btn-danger btn-xs'
+              onClick={() => deleteBtn(index)} // Pass the index to deleteBtn function
+            >
+              Rejected
+            </button>
+          </div>
+          <div className='card-footer'>
+            <small className='text-muted'>{`Last updated ${time} ago`}</small>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
